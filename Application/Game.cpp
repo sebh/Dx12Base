d2058fdc0005046f24bc23ed7b6114ee75ed1e25
SebH <hillaire.sebastien@gmail.com>
2020-12-13 20:22:48 +0100

    Added cube.obj

--- Application/Game.cpp
+++ Application/Game.cpp
@@ -166,7 +166,7 @@ void Game::initialise()
 	{
 		objl::Loader loader;
 		bool success = loader.LoadFile(".\\Resources\\sphere.obj");
-		//bool success = loader.LoadFile("./Resources/cube.obj");
+		//bool success = loader.LoadFile(".\\Resources\\cube.obj");
 		if (success && loader.LoadedMeshes.size() == 1)
 		{
 			SphereVertexCount = (uint)loader.LoadedVertices.size();
